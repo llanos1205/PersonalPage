87105356fcf9ae177b6e584d49ffd472f9e89e94
llanos1205 <llanos1205@gmail.com>
2020-09-24 23:20:27 -0400

    list

---
+++ src/components/KnowledgeItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="Item_Panel">
+    <div>
+      <img src="src/assets/python.png" alt="python logo">
+    </div>
+
+    <div>
+      <strong>{{ state.name }}</strong>
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive} from "@vue/reactivity";
+
+export default {
+  name: "KnowledgeItem",
+  props: {
+    name: String,
+    value: Number
+  },
+  setup(props) {
+    const state = reactive({
+      name: props.name,
+      value: 0,
+      logo: ""
+    })
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Item_Panel {
+  border: 1px solid dimgray;
+  border-radius: 15px;
+  background-color: #2c3e50;
+  max-width: 200px;
+  max-height: 300px;
+  padding: 100px;
+  margin: 5px;
+  transition: all 0.50s ease;
+}
+
+.Item_Panel:hover {
+  transform: scale(1.05, 1.05);
+}
+
+strong {
+  color: white;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="Knowledge_holder">
+      <knowledge-item v-for="(item,index) in state.items" :name="item.name" :value="item.value" :key="index"></knowledge-item>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import KnowledgeItem from "@/components/KnowledgeItem";
+import {reactive} from "@vue/reactivity";
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    KnowledgeItem
+  },
+  setup() {
+    const state = reactive({
+      items: [
+        {
+          name: "python",
+          logo: "",
+          value: 95,
+          extras: []
+        },
+        {
+          name: "c#",
+          logo: "",
+          value: 75,
+          extras:[]
+        },
+        {
+          name:"JavaScript",
+          logo:"",
+          value:15,
+          extras:[]
+        }
+      ]
+    })
+    return {
+      state
+    }
+  }
+}
+</script>
+<style scoped>
+.Knowledge_holder{
+  display: flex;
+  flex-direction: row;
+}
+</style>
